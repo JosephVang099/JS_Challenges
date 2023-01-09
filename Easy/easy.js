1b55db7a1035d3def4eb3b92620d85ae70549460
JosephVang099 <joseph-vang@outlook.com>
2023-01-09 13:15:58 -0500

    Create easy.js

---
+++ Easy/easy.js
@@ -0,0 +1,7 @@
+// Create two variables and assign a person’s name to each. Console log a statement that says which name is shorter or longer, and by how many characters.                                                       Example output: “The name Thomas is longer than John by 2 characters”
+
+
+var name1 = "Thomas"
+var name2 = "John"
+
+console.log("The name " + name1 + " is longer than " + name2 + " by " + (name1.length - name2.length) + " characters")
